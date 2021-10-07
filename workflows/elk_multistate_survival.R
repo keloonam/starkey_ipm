81f306eabe50f0ce65ae8b6fb32d318ee0957f4c
Kenneth Loonam <keloonam@gmail.com>
2021-10-07 14:26:00 -0700

    Added multistate survival model workflow and debugged the multistate survival model. Ready to run in JAGS and estimate separate natural and harvest survival.

--- workflows/elk_multistate_survival.R
+++ workflows/elk_multistate_survival.R
@@ -18,14 +18,16 @@ params <- c(
 # Tau for logistic transformed priors
 pr_p <- 0.5
 
-# Model
-model_file <- "modells//survival//survival_multi_state_model.txt"
+# File names/paths
+model_file <- "models//survival//survival_multi_state_model.txt"
+result_file <- "results//survival//survival_multistate_result.Rdata"
+
 
 # Sampler variables
-ni <- 100
-nt <- 1
-nb <- 20
-nc <- 3
+n_i <- 10000
+n_t <- 1
+n_b <- 20000
+n_c <- 3
 
 #Environment====================================================================
 
@@ -51,7 +53,9 @@ y <- elk_data$cap_tib %>%
   arrange(id) %>%
   select(as.character(start_year:end_year)) %>%
   as.matrix()
+
 f <- apply(y, 1, function(x) min(which(x != 0)))
+
 l <- elk_data$hnt_tib %>%
   filter(id %in% elk_data$cap_tib$id) %>%
   arrange(id) %>%
@@ -59,56 +63,75 @@ l <- elk_data$hnt_tib %>%
   select(as.character(start_year:end_year)) %>%
   as.matrix() %>%
   apply(., 1, function(x) min(which(x != 0)))
+
+w <- elk_data$hnt_tib %>%
+  filter(id %in% elk_data$cap_tib$id) %>%
+  arrange(id) %>%
+  select(as.character(start_year:end_year)) %>%
+  as.matrix() %>%
+  magrittr::subtract(1) %>%
+  abs()
+
+for(i in 1:length(l)){
+  y[i,l[i]] <- 0
+}
+
 male <- elk_data$sex_tib %>%
   filter(id %in% elk_data$cap_tib$id) %>%
   arrange(id) %>%
   mutate(male = as.numeric(Sex == "M")) %>%
-  pull(male)
+  pull(male) %>%
+  magrittr::add(1)
+
 calf <- elk_data$age_tib %>%
   filter(id %in% elk_data$cap_tib$id) %>%
   arrange(id) %>%
   select(as.character(start_year:end_year)) %>%
-  as.matrix()
-herds <- elk_data$hrd_tib %>%
+  as.matrix() %>%
+  replace_na(0) %>%
+  magrittr::subtract(2) %>%
+  abs()
+
+herd <- elk_data$hrd_tib %>%
   filter(id %in% elk_data$cap_tib$id) %>%
   arrange(id) %>%
   select(as.character(start_year:end_year)) %>%
+  mutate_all(funs(case_when(
+    . == "main" ~ 1,
+    . == "ne_e" ~ 2,
+    . == "ne_w" ~ 3,
+    . == "ne_o" ~ 4,
+    . == "camp" ~ 5,
+    . == "otsd" ~ 6,
+    . == "hand" ~ 7
+  ))) %>%
   as.matrix()
+
 bad_elk <- which(f == l)
 y <- y[-bad_elk,]
 f <- f[-bad_elk]
 l <- l[-bad_elk]
+w <- w[-bad_elk,]
 male <- male[-bad_elk]
 calf <- calf[-bad_elk,]
-herds <- herds[-bad_elk,]
-camp <- herds == "camp"
-hand <- herds == "hand"
-ne_e <- herds == "ne_e"
-ne_o <- herds == "ne_o"
-ne_w <- herds == "ne_w"
-otsd <- herds == "otsd"
+herd <- herd[-bad_elk,]
 
-
-elk_cjs_data <- list(
+jags_data <- list(
   y = y,
   f = f,
   l = l,
+  w = w,
   male = male,
   calf = calf,
-  camp = camp,
-  hand = hand,
-  ne_e = ne_e,
-  ne_o = ne_o,
-  ne_w = ne_w,
-  otsd = otsd,
+  herd = herd,
   n_ind = nrow(y),
   n_occ = ncol(y),
   pr_p = pr_p
 )
 
-z <- ch_init_fn(elk_cjs_data$y, elk_cjs_data$f)
+z <- ch_init_fn(jags_data$y, jags_data$f)
 
-model_inits <- function(){
+inits <- function(){
   list(
     z = z
   )
@@ -117,15 +140,20 @@ model_inits <- function(){
 #Fit_model======================================================================
 
 # run the MCMC chain in JAGS
-rslt <- jags(
-  data = elk_cjs_data, 
-  inits = model_inits,
-  parameters.to.save = params,
-  model.file = model_file,
-  n.chains = nc,
-  n.iter = ni,
-  n.burnin = nb,
-  n.thin = nt
+jgs_mdl <- jags.model(
+  file = model_file,
+  data = jags_data,
+  inits = inits,
+  n.chains = n_c
 )
-mcmcplot(rslt)
-save(rslt, file = "rand_effect.Rdata")
+
+update(jgs_mdl, n.iter = n_b)
+
+rslt <- coda.samples(
+  jgs_mdl,
+  variable.names = params,
+  n.iter = n_i,
+  thin = n_t
+)
+
+summary(rslt)
