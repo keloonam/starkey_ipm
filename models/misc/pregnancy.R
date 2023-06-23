2e2ad8f45b93ffe385a24c50880707175210583b
Kenneth Loonam <keloonam@gmail.com>
2023-06-23 09:58:53 -0700

    updated pregnancy data cleaning and analysis following starkey collab meeting

--- models/misc/pregnancy.R
+++ models/misc/pregnancy.R
@@ -1,27 +1,59 @@
 nimble_code <- nimble::nimbleCode({
   
   # Priors
-  b0_mean ~ dlogis(0, 1)
-  b0_sd   ~ T(dnorm(0, sd = 10), 0, 10)
-  b_lact_mean ~ dlogis(0, 1)
-  b_lact_sd   ~ T(dnorm(0, sd = 10), 0, 10)
+  b0_mean        ~ dlogis(0, 1)
+  b_yng_mean     ~ dlogis(0, 1)
+  b_old_mean     ~ dlogis(0, 1)
+  b_lac_prm_mean ~ dlogis(0, 1)
+  b_lac_yng_mean ~ dlogis(0, 1)
+  b_lac_old_mean ~ dlogis(0, 1)
+  b0_sd          ~ T(dnorm(0, sd = 10), 0, 10)
+  b_yng_sd       ~ T(dnorm(0, sd = 10), 0, 10)
+  b_old_sd       ~ T(dnorm(0, sd = 10), 0, 10)
+  b_lac_prm_sd   ~ T(dnorm(0, sd = 10), 0, 10)
+  b_lac_yng_sd   ~ T(dnorm(0, sd = 10), 0, 10)
+  b_lac_old_sd   ~ T(dnorm(0, sd = 10), 0, 10)
   for(t in 1:ny){
-    b0[t]     ~ dnorm(b0_mean, sd = b0_sd)
-    b_lact[t] ~ dnorm(b_lact_mean, sd = b_lact_sd)
+    b0[t]        ~ dnorm(b0_mean,        sd = b0_sd)
+    b_yng[t]     ~ dnorm(b_yng_mean,     sd = b_yng_sd)
+    b_old[t]     ~ dnorm(b_old_mean,     sd = b_old_sd)
+    b_lac_prm[t] ~ dnorm(b_lac_prm_mean, sd = b_lac_prm_sd)
+    b_lac_yng[t] ~ dnorm(b_lac_yng_mean, sd = b_lac_yng_sd)
+    b_lac_old[t] ~ dnorm(b_lac_old_mean, sd = b_lac_old_sd)
   }
-  b_dens_open ~ dlogis(0, 1)
-  b_pdsi_open ~ dlogis(0, 1)
-  b_dens_lact ~ dlogis(0, 1)
-  b_pdsi_lact ~ dlogis(0, 1)
+  bden_dry_prm ~ dlogis(0, 1)
+  bden_dry_yng ~ dlogis(0, 1)
+  bden_dry_old ~ dlogis(0, 1)
+  bpdi_dry_prm ~ dlogis(0, 1)
+  bpdi_dry_yng ~ dlogis(0, 1)
+  bpdi_dry_old ~ dlogis(0, 1)
+  bden_lac_prm ~ dlogis(0, 1)
+  bden_lac_yng ~ dlogis(0, 1)
+  bden_lac_old ~ dlogis(0, 1)
+  bpdi_lac_prm ~ dlogis(0, 1)
+  bpdi_lac_yng ~ dlogis(0, 1)
+  bpdi_lac_old ~ dlogis(0, 1)
   
   # GLM
   for(i in 1:nr){
     logit(p[i]) <- b0[yr[i]] + 
-      b_lact[yr[i]] * lac[i] +
-      b_dens_open   * elk[i] * (1 - lac[i]) +
-      b_pdsi_open   * pdi[i] * (1 - lac[i]) +
-      b_dens_lact   * elk[i] *      lac[i]  +
-      b_pdsi_lact   * pdi[i] *      lac[i]
+      b_yng[yr[i]]                             * yng[i] +
+      b_old[yr[i]]                             * old[i] +
+      b_lac_prm[yr[i]]          *      lac[i]  * prm[i] +
+      b_lac_yng[yr[i]]          *      lac[i]  * yng[i] +
+      b_lac_old[yr[i]]          *      lac[i]  * old[i] +
+      bden_dry_prm * elk[yr[i]] * (1 - lac[i]) * prm[i] +          
+      bden_dry_yng * elk[yr[i]] * (1 - lac[i]) * yng[i] +
+      bden_dry_old * elk[yr[i]] * (1 - lac[i]) * old[i] +
+      bpdi_dry_prm * pdi[yr[i]] * (1 - lac[i]) * prm[i] +          
+      bpdi_dry_yng * pdi[yr[i]] * (1 - lac[i]) * yng[i] +
+      bpdi_dry_old * pdi[yr[i]] * (1 - lac[i]) * old[i] + 
+      bden_lac_prm * elk[yr[i]] *      lac[i]  * prm[i] +          
+      bden_lac_yng * elk[yr[i]] *      lac[i]  * yng[i] +
+      bden_lac_old * elk[yr[i]] *      lac[i]  * old[i] +
+      bpdi_lac_prm * pdi[yr[i]] *      lac[i]  * prm[i] +          
+      bpdi_lac_yng * pdi[yr[i]] *      lac[i]  * yng[i] +
+      bpdi_lac_old * pdi[yr[i]] *      lac[i]  * old[i]
 
   # Model  
     preg[i] ~ dbin(p[i], nobs[i])
--- workflows/model_runs/pregnancy_analyses.R
+++ workflows/model_runs/pregnancy_analyses.R
@@ -4,8 +4,8 @@
 
 #Variables======================================================================
 
-niter <- 50000
-nburn <- 10000
+niter <- 100000
+nburn <- 50000
 chain <- 3
 
 #Packages=======================================================================
@@ -13,7 +13,7 @@ chain <- 3
 require(tidyverse); require(nimble)
 
 #Load stuff=====================================================================
-rd <- read.csv("data//pregnancy_lactation_2002_present.csv") %>%
+rd <- read.csv("data//pregnancy_rates.csv") %>%
   as_tibble()
 
 load("data//elk_ipm_data_21apr2023.Rdata")
@@ -22,68 +22,82 @@ source("models//misc//pregnancy.R")
 
 #Prep data======================================================================
 
-preg_rates <- rd %>% group_by(HandlingYr, Lactating) %>%
-  summarise(
-    pregnancy_rate = mean(Pregnant, na.rm = T),
-    pregnancy_sd = sd(Pregnant, na.rm = T),
-    n_observations = n()
-    ) %>%
-  filter(Lactating == "F" | Lactating == "T") %>%
-  mutate(lactating = Lactating == "T") %>%
-  ungroup() %>%
-  separate(HandlingYr, c("year", "yr2")) %>%
-  select(year, lactating, pregnancy_rate, pregnancy_sd, n_observations) %>%
-  mutate(id = 1:nrow(.))
-
-full_dat <- tibble(
-  pdi = rep(ipm_data$palmer_index[15:34], 2),
-  elk = rep(ipm_data$elk_density[15:34], 2),
-  yr = rep(2002:2021, 2)
-) %>%
-  arrange(yr) %>%
-  mutate(id = 1:nrow(.)) %>%
-  full_join(preg_rates)
+cov_dat <- tibble(
+  pdi = ipm_data$palmer_index[2:34],
+  elk = ipm_data$elk_density[2:34],
+  yr  = (1989:2021) - 1988
+)
 
 #Prep nimble====================================================================
 
 nimble_data <- list(
-  lac  = full_dat$lactating * 1,
-  nobs = full_dat$n_observations,
-  preg = round(full_dat$pregnancy_rate * full_dat$n_observations),
-  pdi  = full_dat$pdi,
-  elk  = full_dat$elk
+  nobs = rd$n_observations,
+  preg = round(rd$pregnancy_rate * rd$n_observations)
 )  
 
 nimble_constants <- list(
-  nr = nrow(full_dat),
-  ny = length(unique(full_dat$year)),
-  yr   = sort(rep(1:length(unique(full_dat$year)), 2))
+  nr = nrow(rd),
+  ny = max(rd$yr) - min(rd$yr) + 1,
+  yr = rd$yr - 1988,
+  yng  = rd$age_class == "young",
+  old  = rd$age_class == "old",
+  prm  = rd$age_class == "prime",
+  pdi  = cov_dat$pdi,
+  elk  = cov_dat$elk,
+  lac  = rd$lactating * 1
 )
 
 nimble_inits <- function(){
   list(
     b0_mean = rlogis(1, 0, 1),
     b0_sd = runif(1, 0, 10),
-    b0 = rlogis(20, 0, 1),
-    b_lact_mean = rlogis(1, 0, 1),
-    b_lact_sd = runif(1, 0, 10),
-    b_lact = rlogis(20, 0, 1),
-    b_dens_open = rlogis(1, 0, 1),
-    b_pdsi_open = rlogis(1, 0, 1),
-    b_dens_lact = rlogis(1, 0, 1),
-    b_pdsi_lact = rlogis(1, 0, 1)
+    b0 = rlogis(33, 0, 1),
+    b_yng_mean = rlogis(1, 0, 1),
+    b_old_mean = rlogis(1, 0, 1),
+    b_yng_sd = runif(1, 0, 10),
+    b_old_sd = runif(1, 0, 10),
+    b_yng = rlogis(33, 0, 1),
+    b_old = rlogis(33, 0, 1),
+    b_den_dry_prm = rlogis(1, 0, 1),
+    b_den_dry_yng = rlogis(1, 0, 1),
+    b_den_dry_old = rlogis(1, 0, 1),
+    b_pdi_dry_prm = rlogis(1, 0, 1),
+    b_pdi_dry_yng = rlogis(1, 0, 1),
+    b_pdi_dry_old = rlogis(1, 0, 1),
+    b_den_lac_prm = rlogis(1, 0, 1),
+    b_den_lac_yng = rlogis(1, 0, 1),
+    b_den_lac_old = rlogis(1, 0, 1),
+    b_pdi_lac_prm = rlogis(1, 0, 1),
+    b_pdi_lac_yng = rlogis(1, 0, 1),
+    b_pdi_lac_old = rlogis(1, 0, 1)
   )
 }
 
 nimble_monitors <- c(
   "b0_mean",
   "b0_sd",
-  "b_lact_mean",
-  "b_lact_sd",
-  "b_dens_open",
-  "b_pdsi_open",
-  "b_dens_lact",
-  "b_pdsi_lact"
+  "b_yng_mean",
+  "b_old_mean",
+  "b_lac_prm_mean",
+  "b_lac_yng_mean",
+  "b_lac_old_mean",
+  "b_yng_sd",
+  "b_old_sd",
+  "b_lac_prm_sd",
+  "b_lac_yng_sd",
+  "b_lac_old_sd",
+  "bden_dry_prm",
+  "bden_dry_yng",
+  "bden_dry_old",
+  "bpdi_dry_prm",
+  "bpdi_dry_yng",
+  "bpdi_dry_old",
+  "bden_lac_prm",
+  "bden_lac_yng",
+  "bden_lac_old",
+  "bpdi_lac_prm",
+  "bpdi_lac_yng",
+  "bpdi_lac_old"
 )
 
 #Run model======================================================================
@@ -100,3 +114,90 @@ nimble_results <- nimble::nimbleMCMC(
 )
   
 mcmcplots::mcmcplot(nimble_results)
+
+#Clean results==================================================================
+
+row_names <- dimnames(nimble_results[[1]])[[2]]
+
+cov_dat <- nimble_results %>%
+  map(as_tibble) %>%
+  bind_rows() %>%
+  map(quantile, probs = c(.025, .5, .975)) %>%
+  bind_rows() %>%
+  mutate(model_alias = row_names) %>%
+  mutate(keep = grepl("bden", model_alias) | grepl("bpdi", model_alias)) %>%
+  filter(keep == T) %>%
+  mutate(lactating = case_when(
+    grepl("dry", model_alias) ~ "Not lactating",
+    grepl("lac", model_alias) ~ "Lactating"
+  )) %>%
+  mutate(age = case_when(
+    grepl("old", model_alias) ~ "Old",
+    grepl("yng", model_alias) ~ "Young",
+    grepl("prm", model_alias) ~ "Prime aged"
+  )) %>%
+  mutate(covariate = case_when(
+    grepl("den", model_alias) ~ "Elk density",
+    grepl("pdi", model_alias) ~ "Palmer drought index"
+  )) %>%
+  mutate(lci = `2.5%`) %>%
+  mutate(uci = `97.5%`) %>%
+  mutate(med = `50%`) %>%
+  select(lactating, age, covariate, lci, med, uci) %>%
+  filter(age != "Young" | lactating != "Lactating")
+
+prg_dat <- nimble_results %>%
+  map(as_tibble) %>%
+  bind_rows() %>%
+  mutate(young = b0_mean + b_yng_mean) %>%
+  mutate(old_lactating = b0_mean + b_old_mean + b_lac_old_mean) %>%
+  mutate(old_open = b0_mean + b_old_mean) %>%
+  mutate(prime_lactating = b0_mean + b_lac_prm_mean) %>%
+  mutate(prime_open = b0_mean) %>%
+  select(prime_open, prime_lactating, old_open, old_lactating, young) %>%
+  pivot_longer(cols = everything(), names_to = "Age/lactation class", values_to = "val")  %>%
+  mutate(val = exp(val)/(1+exp(val))) %>%
+  mutate(`Age/lactation class` = case_when(
+    `Age/lactation class` == "old_lactating" ~ "Old - lactating",
+    `Age/lactation class` == "old_open" ~ "Old",
+    `Age/lactation class` == "prime_lactating" ~ "Prime - lactating",
+    `Age/lactation class` == "prime_open" ~ "Prime",
+    `Age/lactation class` == "young" ~ "Young",
+  ))
+
+#Plot results===================================================================
+
+ggplot(cov_dat, aes(x = covariate, y = med, color = age, shape = lactating)) +
+  geom_pointrange(aes(ymin = lci, ymax = uci), position = position_dodge(width = .5)) +
+  theme(legend.title = element_blank()) +
+  labs(x = "", y = "Covariate value", title = "Effects on pregnancy rates") +
+  theme_classic() +
+  geom_hline(yintercept = 0, linetype = "dotted")
+  
+source("https://raw.githubusercontent.com/datavizpyr/data/master/half_flat_violinplot.R")
+
+ggplot(prg_dat, aes(x = `Age/lactation class`, y = val, color = `Age/lactation class`, fill = `Age/lactation class`)) +
+  geom_flat_violin() +
+  theme_classic() +
+  theme(legend.position = "none") +
+  labs(x = "", y = "Posterior distribution", title = "Mean pregnancy rates") +
+  coord_flip()
+
+samp_size <- rd %>%
+  mutate(ratio = paste0(
+    " ",
+    round(pregnancy_rate * n_observations),
+    "/",
+    n_observations
+    )) %>%
+  mutate(class = case_when(
+    age_class == "old" & lactating == TRUE ~ "Old - lactating",
+    age_class == "old" & lactating == FALSE ~ "Old",
+    age_class == "prime" & lactating == TRUE ~ "Prime - lactating",
+    age_class == "prime" & lactating == FALSE ~ "Prime",
+    age_class == "young" & lactating == FALSE ~ "Young",
+    age_class == "young" & lactating == TRUE ~ "Young - lactating"
+  )) %>%
+  select(yr, ratio, class) %>%
+  pivot_wider(names_from = class, values_from = ratio) %>%
+  write.csv("data//pregnancy_sample_sizes.csv")
