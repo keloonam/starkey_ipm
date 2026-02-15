7c21bc934b781a72b67354ee6090096ad62871e8
keloonam <keloonam@gmail.com>
2026-02-15 02:55:52 +0000

    added to writing scratch work. New figure script to expand comparison of lambda contributions.

---
+++ workflows/figure_prep/compare_elasticity_SI_fig.R
@@ -0,0 +1,285 @@
+require(tidyverse)
+save_file_name <-  "figures//elasticity_comparison_SI.png"
+tag <- "best_lgst"
+edt_name <- paste0("results//lambda_elasticity_", tag, ".rds")
+sdt_name <- paste0("results//lambda_sensitivity_", tag, ".rds")
+edt <- readRDS(edt_name)
+sdt <- readRDS(sdt_name)
+total_variation_by_step <- edt$wvcv %>%
+  mutate(val = abs(value)) %>%
+  summarise(tot = sum(val), .by = stpn)
+lgst_dt <- edt$wvcv %>%
+  pivot_wider(id_cols = stpn, names_from = param, values_from = value) %>%
+  left_join(total_variation_by_step) %>%
+  select(
+    stpn, tot, 
+    cg, dd, wm, wt, 
+    prc, pr2, pry, pro, prp,
+    sh1, sh2,
+    ppyr, snyr, scyr, sfyr
+  ) %>%
+  pivot_longer(3:ncol(.), names_to = "param", values_to = "val") %>%
+  mutate(value = abs(val) / tot) %>%
+  group_by(param) %>%
+  summarise(
+    lci = quantile(value, 0.025),
+    mci = quantile(value, 0.5),
+    uci = quantile(value, 0.975)
+  ) %>%
+  ungroup() %>%
+  mutate(prm = case_when(
+    param == "dd" ~ "l dd",
+    param == "wt" ~ "m wt",
+    param == "wm" ~ "n wm",
+    param == "cg" ~ "o cg",
+    param == "ppyr" ~ "a pregnancy",
+    param == "snyr" ~ "b neonate survival",
+    param == "scyr" ~ "c calf survival",
+    param == "sfyr" ~ "d adult survival",
+    param == "sh1" ~ "e calf harvest",
+    param == "sh2" ~ "f adult harvest",
+    param == "prc" ~ "g calf",
+    param == "pr2" ~ "h yearling",
+    param == "pry" ~ "i young",
+    param == "prp" ~ "j prime",
+    param == "pro" ~ "k old"
+  )) %>%
+  mutate(tag = tag)
+
+tag <- "best_mean"
+edt_name <- paste0("results//lambda_elasticity_", tag, ".rds")
+sdt_name <- paste0("results//lambda_sensitivity_", tag, ".rds")
+edt <- readRDS(edt_name)
+sdt <- readRDS(sdt_name)
+total_variation_by_step <- edt$wvcv %>%
+  mutate(val = abs(value)) %>%
+  summarise(tot = sum(val), .by = stpn)
+mean_dt <- edt$wvcv %>%
+  pivot_wider(id_cols = stpn, names_from = param, values_from = value) %>%
+  left_join(total_variation_by_step) %>%
+  select(
+    stpn, tot, 
+    cg, dd, wm, wt, 
+    prc, pr2, pry, pro, prp,
+    sh1, sh2,
+    ppyr, snyr, scyr, sfyr
+  ) %>%
+  pivot_longer(3:ncol(.), names_to = "param", values_to = "val") %>%
+  mutate(value = abs(val) / tot) %>%
+  group_by(param) %>%
+  summarise(
+    lci = quantile(value, 0.025),
+    mci = quantile(value, 0.5),
+    uci = quantile(value, 0.975)
+  ) %>%
+  ungroup() %>%
+  mutate(prm = case_when(
+    param == "dd" ~ "l dd",
+    param == "wt" ~ "m wt",
+    param == "wm" ~ "n wm",
+    param == "cg" ~ "o cg",
+    param == "ppyr" ~ "a pregnancy",
+    param == "snyr" ~ "b neonate survival",
+    param == "scyr" ~ "c calf survival",
+    param == "sfyr" ~ "d adult survival",
+    param == "sh1" ~ "e calf harvest",
+    param == "sh2" ~ "f adult harvest",
+    param == "prc" ~ "g calf",
+    param == "pr2" ~ "h yearling",
+    param == "pry" ~ "i young",
+    param == "prp" ~ "j prime",
+    param == "pro" ~ "k old"
+  )) %>%
+  mutate(tag = tag)
+
+tag <- "best_norm"
+edt_name <- paste0("results//lambda_elasticity_", tag, ".rds")
+sdt_name <- paste0("results//lambda_sensitivity_", tag, ".rds")
+edt <- readRDS(edt_name)
+sdt <- readRDS(sdt_name)
+total_variation_by_step <- edt$wvcv %>%
+  mutate(val = abs(value)) %>%
+  summarise(tot = sum(val), .by = stpn)
+norm_dt <- edt$wvcv %>%
+  pivot_wider(id_cols = stpn, names_from = param, values_from = value) %>%
+  left_join(total_variation_by_step) %>%
+  select(
+    stpn, tot, 
+    cg, dd, wm, wt, 
+    prc, pr2, pry, pro, prp,
+    sh1, sh2,
+    ppyr, snyr, scyr, sfyr
+  ) %>%
+  pivot_longer(3:ncol(.), names_to = "param", values_to = "val") %>%
+  mutate(value = abs(val) / tot) %>%
+  group_by(param) %>%
+  summarise(
+    lci = quantile(value, 0.025),
+    mci = quantile(value, 0.5),
+    uci = quantile(value, 0.975)
+  ) %>%
+  ungroup() %>%
+  mutate(prm = case_when(
+    param == "dd" ~ "l dd",
+    param == "wt" ~ "m wt",
+    param == "wm" ~ "n wm",
+    param == "cg" ~ "o cg",
+    param == "ppyr" ~ "a pregnancy",
+    param == "snyr" ~ "b neonate survival",
+    param == "scyr" ~ "c calf survival",
+    param == "sfyr" ~ "d adult survival",
+    param == "sh1" ~ "e calf harvest",
+    param == "sh2" ~ "f adult harvest",
+    param == "prc" ~ "g calf",
+    param == "pr2" ~ "h yearling",
+    param == "pry" ~ "i young",
+    param == "prp" ~ "j prime",
+    param == "pro" ~ "k old"
+  )) %>%
+  mutate(tag = tag)
+
+tag <- "best_odfw"
+edt_name <- paste0("results//lambda_elasticity_", tag, ".rds")
+sdt_name <- paste0("results//lambda_sensitivity_", tag, ".rds")
+edt <- readRDS(edt_name)
+sdt <- readRDS(sdt_name)
+total_variation_by_step <- edt$wvcv %>%
+  mutate(val = abs(value)) %>%
+  summarise(tot = sum(val), .by = stpn)
+odfw_dt <- edt$wvcv %>%
+  pivot_wider(id_cols = stpn, names_from = param, values_from = value) %>%
+  left_join(total_variation_by_step) %>%
+  select(
+    stpn, tot, 
+    cg, dd, wm, wt, 
+    prc, pr2, pry, pro, prp,
+    sh1, sh2,
+    ppyr, snyr, scyr, sfyr
+  ) %>%
+  pivot_longer(3:ncol(.), names_to = "param", values_to = "val") %>%
+  mutate(value = abs(val) / tot) %>%
+  group_by(param) %>%
+  summarise(
+    lci = quantile(value, 0.025),
+    mci = quantile(value, 0.5),
+    uci = quantile(value, 0.975)
+  ) %>%
+  ungroup() %>%
+  mutate(prm = case_when(
+    param == "dd" ~ "l dd",
+    param == "wt" ~ "m wt",
+    param == "wm" ~ "n wm",
+    param == "cg" ~ "o cg",
+    param == "ppyr" ~ "a pregnancy",
+    param == "snyr" ~ "b neonate survival",
+    param == "scyr" ~ "c calf survival",
+    param == "sfyr" ~ "d adult survival",
+    param == "sh1" ~ "e calf harvest",
+    param == "sh2" ~ "f adult harvest",
+    param == "prc" ~ "g calf",
+    param == "pr2" ~ "h yearling",
+    param == "pry" ~ "i young",
+    param == "prp" ~ "j prime",
+    param == "pro" ~ "k old"
+  )) %>%
+  mutate(tag = tag)
+
+tag <- "best_rcns"
+edt_name <- paste0("results//lambda_elasticity_", tag, ".rds")
+sdt_name <- paste0("results//lambda_sensitivity_", tag, ".rds")
+edt <- readRDS(edt_name)
+sdt <- readRDS(sdt_name)
+total_variation_by_step <- edt$wvcv %>%
+  mutate(val = abs(value)) %>%
+  summarise(tot = sum(val), .by = stpn)
+rcns_dt <- edt$wvcv %>%
+  pivot_wider(id_cols = stpn, names_from = param, values_from = value) %>%
+  left_join(total_variation_by_step) %>%
+  select(
+    stpn, tot, 
+    cg, dd, wm, wt, 
+    prc, pr2, pry, pro, prp,
+    sh1, sh2,
+    ppyr, snyr, scyr, sfyr
+  ) %>%
+  pivot_longer(3:ncol(.), names_to = "param", values_to = "val") %>%
+  mutate(value = abs(val) / tot) %>%
+  group_by(param) %>%
+  summarise(
+    lci = quantile(value, 0.025),
+    mci = quantile(value, 0.5),
+    uci = quantile(value, 0.975)
+  ) %>%
+  ungroup() %>%
+  mutate(prm = case_when(
+    param == "dd" ~ "l dd",
+    param == "wt" ~ "m wt",
+    param == "wm" ~ "n wm",
+    param == "cg" ~ "o cg",
+    param == "ppyr" ~ "a pregnancy",
+    param == "snyr" ~ "b neonate survival",
+    param == "scyr" ~ "c calf survival",
+    param == "sfyr" ~ "d adult survival",
+    param == "sh1" ~ "e calf harvest",
+    param == "sh2" ~ "f adult harvest",
+    param == "prc" ~ "g calf",
+    param == "pr2" ~ "h yearling",
+    param == "pry" ~ "i young",
+    param == "prp" ~ "j prime",
+    param == "pro" ~ "k old"
+  )) %>%
+  mutate(tag = tag)
+
+
+
+
+bind_rows(lgst_dt, mean_dt, norm_dt, odfw_dt, rcns_dt) %>%
+  ggplot(aes(x = prm, y = mci, color = tag, shape = tag)) +
+  geom_pointrange(aes(ymin = lci, ymax = uci), position = position_dodge(width = .4), size = 0.5) +
+  theme_classic() +
+  scale_x_discrete(
+    labels = c(
+      `l dd` = "Density",
+      `m wt` = bquote("SPEI"[t]),
+      `n wm` = bquote("SPEI"[t-1]),
+      `o cg` = "Pumas",
+      `a pregnancy` = "Pregnancy",
+      `b neonate survival` = "S (neonate)",
+      `c calf survival` = "S (calf)",
+      `d adult survival` = "S (adult)",
+      `e calf harvest` = "H (calf)",
+      `f adult harvest` = "H (adult)",
+      `g calf` = "PN (calf)",
+      `h yearling` = "PN (yearling)",
+      `i young` = "PN (young)",
+      `j prime` = "PN (prime)",
+      `k old` = "PN (old)"
+    ),
+    guide = guide_axis(n.dodge = 2)
+  ) +
+  scale_color_discrete(
+    labels = c(
+      best_lgst = "Logistic Growth",
+      best_mean = "Index Mean",
+      best_norm = "Normal Draw",
+      best_odfw = "ODFW Estimate",
+      best_rcns = "Reconstruction"
+  )) +
+  scale_shape_discrete(
+    labels = c(
+      best_lgst = "Logistic Growth",
+      best_mean = "Index Mean",
+      best_norm = "Normal Draw",
+      best_odfw = "ODFW Estimate",
+      best_rcns = "Reconstruction"
+    )) +
+  xlab(NULL) + ylab("Contribution") +
+  labs(color = element_blank(), shape = element_blank()) +
+  theme(legend.position = "bottom") 
+ggsave(
+  filename = paste0(save_file_name),
+  width = 18,
+  height = 10,
+  units = "cm",
+  dpi = 600
+)
--- writing_scratch_work.R
+++ writing_scratch_work.R
@@ -1,3 +1,5 @@
+require(tidyverse); require(rjags)
+ipmdt <- readRDS("data//ipm_data_03feb2026.rds")
 lgstrs <- readRDS("results//fbipm_rslt_06feb2026_full_lgst.rds") %>%
   map(as.matrix) %>% map(as_tibble) %>% bind_rows()
 meanrs <- readRDS("results//fbipm_rslt_06feb2026_full_mean.rds") %>%
@@ -28,11 +30,21 @@ param_summ <- function(fdt, param){
     quantile(c(0.025, 0.5, 0.975)) %>%
     return()
 }
-rs1 <- lgstrs %>% param_summ(PB_dd); lgstrs %>% param_summ(PB_wm)
-rs2 <- meanrs %>% param_summ(PB_dd); meanrs %>% param_summ(PB_wm)
-rs3 <- normrs %>% param_summ(PB_dd); normrs %>% param_summ(PB_wm)
-rs4 <- odfwrs %>% param_summ(PB_dd); odfwrs %>% param_summ(PB_wm)
-rs5 <- rcnsrs %>% param_summ(PB_dd); rcnsrs %>% param_summ(PB_wm)
+expit <- function(x){
+  1/(1+exp(-x))
+}
+ann_mean <- function(fdt, param_pattern){
+  fdt %>%
+    select(grep(param_pattern, names(.))) %>%
+    as.matrix() %>% colMeans() %>% mean() %>%
+    return()
+}
+
+lgstrs %>% param_summ(SNB_dd); lgstrs %>% param_summ(SNB_cg)
+meanrs %>% param_summ(SNB_dd); meanrs %>% param_summ(SNB_cg)
+normrs %>% param_summ(SNB_dd); normrs %>% param_summ(SNB_cg)
+odfwrs %>% param_summ(SNB_dd); odfwrs %>% param_summ(SNB_cg)
+rcnsrs %>% param_summ(SNB_dd); rcnsrs %>% param_summ(SNB_cg)
 
 full_param_summ <- function(fdt_list, param){
   rs1 <- fdt_list$lgst %>% param_summ({{param}})
@@ -43,4 +55,10 @@ full_param_summ <- function(fdt_list, param){
   
   rbind(rs1, rs2, rs3, rs4, rs5) %>% colMeans() %>% round() %>% return()
 }
-full_param_summ(rslt_list, PB_dd)
+full_param_summ(rslt_list, SFB_wm)
+ann_mean(meanrs, "N_f\\[") * ann_mean(meanrs, "Pbar")
+sncg <- meanrs %>% pull(SNB_cg)
+snb0 <- meanrs %>% pull(SN_B0)
+expit(snb0 + sncg*ipmdt$cg_mean_estimate[1]) %>% mean()
+expit(snb0 + sncg*ipmdt$cg_mean_estimate[20]) %>% mean()
+188*0.73 - 188*0.46
